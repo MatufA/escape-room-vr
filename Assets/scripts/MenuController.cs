9e0de874a1e75768947850de4f553ab8dcc2030c
adiel <amatuf@tremorvideo.com>
2022-01-08 22:34:40 +0200

    fix teleportation error

--- Assets/scripts/MenuController.cs
+++ Assets/scripts/MenuController.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    string code = "";
+    public int codeLength;
+    public Text intoClue;
+    public LockScreen lockCode;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        for (int i = 0; i < codeLength; i++)
+        {
+            code += ((uint)Random.Range(1, 10)).ToString();
+        }
         
     }
 
     public void StartButton()
     {
-        Debug.Log("Start button clicked");
+        lockCode.code = code;
+        intoClue.text = code;
+        Debug.Log("code is " + code);
     }
 }
